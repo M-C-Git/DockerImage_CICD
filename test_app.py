e73fdfef7fff3af069746aa418db0b9cc1cbe612
MChen-AI <peal888.pc@gmail.com>
2025-06-02 13:00:58 -0400

    Commit to upload the below files:   -- CICD,yml   -- Dockerfile

---
+++ test_app.py
@@ -0,0 +1,6 @@
+from app import app
+
+def test_home():
+    response = app.test_client().get("/")
+    assert response.status_code == 200, "The webpage load is not successful!"
+    assert response.data == "Hello World!"
